a0223622cdff3dcaaff345298cf25ed8593ecb37
Giovani Moutinho <giovanim@ymail.com>
2021-03-13 15:08:43 -0300

    Add homepage crawler
    - get a specific call from upwork api that is made when browsing the
    homepage

---
+++ src/crawlers/homepage.py
@@ -0,0 +1,11 @@
+import json
+
+from . import BaseJsonCrawler
+from .constants import UPWORK_BASE_URL
+
+class HomepageCrawler(BaseJsonCrawler):
+    URL_FWH = f'{UPWORK_BASE_URL}freelancers/api/v1/profile/me/fwh'
+
+    def run(self):
+        json_result = self.get_json_from_page(self.URL_FWH)
+        self.save_json_to_file(json_result, 'homepage_fwh.json')
