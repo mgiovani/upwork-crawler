2920913e81a8e65a7a5806db1c94fb4a12bdfafb
Giovani Moutinho <giovanim@ymail.com>
2021-03-14 13:53:24 -0300

    Add model serialization to homepage crawler

--- upwork_crawler/crawlers/homepage/crawler.py
+++ upwork_crawler/crawlers/homepage/crawler.py
@@ -1,11 +1,12 @@
-import json
-
 from crawlers import BaseJsonCrawler
 from crawlers.constants import UPWORK_BASE_URL
+from .models import FWHModel
+
 
 class HomepageCrawler(BaseJsonCrawler):
     URL_FWH = f'{UPWORK_BASE_URL}freelancers/api/v1/profile/me/fwh'
 
     def run(self):
         json_result = self.get_json_from_page(self.URL_FWH)
-        self.save_json_to_file(json_result, 'homepage_fwh.json')
+        model = FWHModel(**json_result)
+        self.save_json_to_file(model.json(), 'homepage_fwh.json')
