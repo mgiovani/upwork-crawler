8b166b3767872ba51428a87f0011ba148e77c5d5
Giovani Moutinho <giovanim@ymail.com>
2021-03-19 18:21:50 -0300

    Fix possible memory leak

--- upwork_crawler/main.py
+++ upwork_crawler/main.py
@@ -6,23 +6,29 @@ from crawlers.profile import ProfileCrawler
 
 
 def main():
-    logger.level('INFO')
-    logger.info('Starting login crawler...')
-    login_crawler = Login()
-    login_crawler.run()
-    web_driver = login_crawler.get_web_driver()
+    web_driver = None
+    try:
+        logger.level('INFO')
+        logger.info('Starting login crawler...')
+        login_crawler = Login()
+        login_crawler.run()
+        web_driver = login_crawler.get_web_driver()
 
-    logger.info('Starting homepage crawler...')
-    homepage_crawler = HomepageCrawler(web_driver)
-    fwh_model = homepage_crawler.run()
+        logger.info('Starting homepage crawler...')
+        homepage_crawler = HomepageCrawler(web_driver)
+        fwh_model = homepage_crawler.run()
 
-    profile_cipher = fwh_model.identity.ciphertext
-    logger.info('Starting profile crawler...')
-    profile_crawler = ProfileCrawler(web_driver)
-    profile_crawler.profile_cipher = profile_cipher
-    profile_crawler.run()
-
-    web_driver.quit()
+        profile_cipher = fwh_model.identity.ciphertext
+        logger.info('Starting profile crawler...')
+        profile_crawler = ProfileCrawler(web_driver)
+        profile_crawler.profile_cipher = profile_cipher
+        profile_crawler.run()
+    except Exception as exc:
+        logger.error(f'An error has ocurred: {exc}')
+        raise exc
+    finally:
+        if web_driver:
+            web_driver.quit()
 
 
 if __name__ == '__main__':
